b024057de813b6b8add34182c060b61c0ed7cabd
follow39 <ingang39@gmail.com>
2020-11-14 09:23:01 +0300

    push

---
+++ Assets/Scripts/DeviceObj.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DeviceObj : MonoBehaviour, IPointerExitHandler
+{
+	Button button;
+
+	// Use this for initialization
+	void Start ()
+	{
+		button = gameObject.transform.Find("Button").GetComponent<Button>();
+		button.onClick.AddListener(OnClickButton);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnClickButton()
+	{
+		gameObject.transform.Find("Panel").gameObject.SetActive(true);
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		gameObject.transform.Find("Panel").gameObject.SetActive(false);
+		Debug.Log("Exit");
+	}
+}
