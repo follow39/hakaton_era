915d1848530d5939ca8be2bf31e33cd1cc9d1f1c
DenAst <astanaev.densi@rambler.ru>
2020-11-14 07:37:26 +0300

    sdf
    df

---
+++ Assets/Scripts/Train/Form_1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Form_1 : MonoBehaviour {
+
+    private string[] inputFields = new string[4]; 
+	public void Cancel()
+    {
+        Destroy(gameObject);
+    }
+    public void Save()
+    {
+        int index = 0;
+        for(int i=0; i<gameObject.transform.childCount;i++)
+        {
+            for(int ii=0; ii<gameObject.transform.GetChild(i).transform.childCount;ii++)
+            {
+                for(int iii=0;iii< gameObject.transform.GetChild(i).transform.GetChild(ii).transform.childCount;iii++)
+                {
+                    if (gameObject.transform.GetChild(i).transform.GetChild(ii).transform.GetChild(iii).name == "input")
+                    {
+                        inputFields[index] = gameObject.transform.GetChild(i).transform.GetChild(ii).transform.GetChild(iii).GetComponent<Text>().text;
+                        index++;
+                    }
+                }
+                
+            }
+        }
+        
+    }
+}
