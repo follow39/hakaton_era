5a485d4e8fe604ff736b06d6baab00d75367c745
follow39 <ingang39@gmail.com>
2020-11-14 16:11:23 +0300

    123

--- Assets/Scripts/Connection.cs
+++ Assets/Scripts/Connection.cs
@@ -14,8 +14,8 @@ public class Connection : MonoBehaviour
 	void Start ()
 	{
 		line = gameObject.GetComponent<LineRenderer>();
-		line.startWidth = 0.1f;
-		line.endWidth = 0.1f;
+		line.startWidth = 0.5f;
+		line.endWidth = 0.5f;
 	}
 	
 	// Update is called once per frame
@@ -50,18 +50,5 @@ public class Connection : MonoBehaviour
 				// line.SetPosition(1, pin2.transform.position);
 			}
 		}
-	}
-	private void DrawLine(Vector2 pointA, Vector2 pointB)
-	{
-		float lineWidth = 2;
-	
-		Vector3 differenceVector = pointB - pointA;
-		// var line = Instantiate(prefabLine, this.GetComponent<Transform>());
-		var imageRectTransform = line.GetComponent<RectTransform>();
-		imageRectTransform.sizeDelta = new Vector2(differenceVector.magnitude / CenterImg.canvas.scaleFactor, lineWidth);
-		imageRectTransform.pivot = new Vector2(0, 0.5f);
-		imageRectTransform.localPosition = new Vector3(pointA.x, pointA.y, CenterImg.transform.position.z);
-		float angle = Mathf.Atan2(differenceVector.y, differenceVector.x) * Mathf.Rad2Deg;
-		line.transform.localRotation = Quaternion.Euler(0, 0, angle);
 	}
 }
